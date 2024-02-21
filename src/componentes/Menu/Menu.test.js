487f3bf8b4eba1ad46dc85bb0999ce981cde0edf
Gustavo Costa <83771792+gustavo-p0@users.noreply.github.com>
2024-02-21 12:13:19 -0300

    course mark: config prettier, eslint, test overview

---
+++ src/componentes/Menu/Menu.test.js
@@ -0,0 +1,26 @@
+import { render, screen } from '@testing-library/react';
+import Menu from './index';
+test('Deve renderizar um link para a página inicial', () => {
+  render(<Menu />);
+  const linkHomePage = screen.getByText('Inicial');
+  expect(linkHomePage).toBeInTheDocument();
+});
+
+test('Deve renderizar uma lista de links', () => {
+  render(<Menu />);
+  const linksList = screen.getAllByRole('link');
+  expect(linksList).toHaveLength(4);
+});
+
+test('Não deve renderizar o link para Extrato', () => {
+  render(<Menu />);
+  const linkExtrato = screen.queryByText('Extrato');
+  expect(linkExtrato).not.toBeInTheDocument();
+});
+
+test('Deve renderizar uma lista de links com a classe link', () => {
+  render(<Menu />);
+  const links = screen.getAllByRole('link');
+  links.forEach((link) => expect(link).toHaveClass('link'));
+  expect(links).toMatchSnapshot();
+});
